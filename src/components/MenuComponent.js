78e1a8f2316479d85bbb1c0bcfccaf24100200d2
NishithD <nishithdasari100@gmail.com>
2022-06-10 03:40:13 +0530

    card improvement

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -5,15 +5,36 @@ class Menu extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            
-        };
+            selectedDish: null            
+        }
     }
 
+    onDishSelect(dish) {
+        this.setState({selectedDish: dish});
+    }
+    renderDish(dish) {
+        if(dish != null) {
+            return(
+                <Card>
+                    <CardImg width="100%" src={dish.image} alt={dish.name} />
+                    <CardBody>
+                        <CardTitle>{dish.name}</CardTitle>
+                        <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+            );
+        }
+        else {
+            return(
+                <div></div>
+            );
+        }
+    }
     render() {
         const menu = this.props.dishes.map((dish) => {
             return (
               <div key={dish.id} className="col-12 col-md-5 m-1">
-                <Card>
+                <Card onClick={() => this.onDishSelect(dish)}>
                       <CardImg width="100%" src={dish.image} alt={dish.name} />
                         <CardImgOverlay>
                                 <CardTitle>{dish.name} </CardTitle>
@@ -28,6 +49,9 @@ class Menu extends Component {
             <div className="row">
                   {menu}
             </div>
+            <div className="row">
+                {this.renderDish(this.state.selectedDish)}
+                </div>
           </div>
         );
     }
