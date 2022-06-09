c07dc1f705770d73c94bc0c745468687d5e5909f
NishithD <nishithdasari100@gmail.com>
2022-06-10 03:10:32 +0530

    app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/MenuComponent';
 import './App.css';
 
 class App extends Component{
@@ -12,6 +12,7 @@ class App extends Component{
             <NavbarBrand href="/">Confusion</NavbarBrand>
           </div>
         </Navbar>
+        <Menu />
       </div>  
     );
   }
